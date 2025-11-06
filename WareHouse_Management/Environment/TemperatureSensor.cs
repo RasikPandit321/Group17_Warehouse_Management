3751f1dba22607dfbf022451366715db474264cc
Bdeuba7977 <bdeuba7977@conestogac.on.ca>
2025-11-06 17:00:37 -0500

    implement TemperatureSensor class to satisfy unit tests AB#19

---
+++ WareHouse_Management/Environment/TemperatureSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WareHouse_Management.Environment
+{
+    public class TemperatureSensor
+    {
+        private readonly Random _random = new Random();
+
+        public double MinTemp { get; }
+        public double MaxTemp { get; }
+
+        public TemperatureSensor(double minTemp, double maxTemp)
+        {
+            if (minTemp >= maxTemp)
+                throw new ArgumentException("minTemp must be less than maxTemp");
+
+            MinTemp = minTemp;
+            MaxTemp = maxTemp;
+        }
+
+        public double ReadTemperature()
+        {
+            double value = MinTemp + _random.NextDouble() * (MaxTemp - MinTemp);
+            return Math.Round(value, 1);   // 1 decimal place
+        }
+    }
+}
+
