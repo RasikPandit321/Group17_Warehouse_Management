02e9f34d21c8301ced0a0e55ecb765bbf081be76
Bdeuba7977 <bdeuba7977@conestogac.on.ca>
2025-11-06 17:01:12 -0500

    add console demo to display live temperature readings AB#20

--- WareHouse_Management/Program.cs
+++ WareHouse_Management/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace WareHouse_Management
+﻿using System;
+using System.Threading.Tasks;
+using WareHouse_Management.Environment;
+
+namespace WareHouse_Management
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sensor = new TemperatureSensor(22, 32);
+
+            Console.WriteLine("Temperature Sensor Demo (5 readings)");
+            Console.WriteLine("-------------------------------------");
+
+            for (int i = 0; i < 5; i++)
+            {
+                double t = sensor.ReadTemperature();
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss}  Temp = {t:F1} °C");
+                await Task.Delay(2000);
+            }
+
+            Console.WriteLine("Done. Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
