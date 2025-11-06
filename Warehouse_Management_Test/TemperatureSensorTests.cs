d41262a79cf55b93414ef56f32e765ceb91eab83
Bdeuba7977 <bdeuba7977@conestogac.on.ca>
2025-11-06 16:59:40 -0500

    add failing unit tests for TemperatureSensor output range and constructor validation AB#18

---
+++ Warehouse_Management_Test/TemperatureSensorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WareHouse_Management.Environment;
+
+namespace Warehouse_Management_Test
+{
+    [TestClass]
+    public class TemperatureSensorTests
+    {
+        [TestMethod]
+        public void ReadTemperature_ReturnsValueWithinConfiguredRange()
+        {
+            // Arrange
+            double min = 10;
+            double max = 40;
+            var sensor = new TemperatureSensor(min, max);
+
+            // Act + Assert
+            for (int i = 0; i < 50; i++)
+            {
+                double t = sensor.ReadTemperature();
+                Assert.IsTrue(t >= min && t <= max,
+                    $"Temperature {t} was outside expected range [{min}, {max}]");
+            }
+        }
+
+        [TestMethod]
+        public void Constructor_Throws_WhenMinIsNotLessThanMax()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() =>
+            {
+                new TemperatureSensor(30, 20);
+            });
+        }
+    }
+}
