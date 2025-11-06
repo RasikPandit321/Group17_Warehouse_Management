804a94bca832d0c01757f12b07deeefe1fc3a017
abhimanyu1188 <arajeevan1188@conestogac.on.ca>
2025-11-06 00:12:21 -0500

    Add DiverterGateController class for gate activation
    Introduced a new `DiverterGateController` class in the
    `WareHouse_Management` namespace to handle diverter gate
    activation. The class includes an `ActivateGate` method
    that logs the movement of packages to a specified zone.
    
    Added necessary `using` directives to support functionality
    and potential future enhancements.

---
+++ WareHouse_Management/DiverterGateController.cs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace WareHouse_Management
+{
+    public class DiverterGateController
+    {
+        public void ActivateGate(string zone)
+        {
+            Console.WriteLine($"🚦 Diverter moving package to {zone}\n");
+        }
+    }
+}
