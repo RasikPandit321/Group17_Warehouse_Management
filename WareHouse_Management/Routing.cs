c6da1090cbbadda51c13bd43f1ea819019773909
abhimanyu1188 <arajeevan1188@conestogac.on.ca>
2025-11-19 15:46:57 -0500

    Add Routing data model with unit test (TDD)

---
+++ WareHouse_Management/Routing.cs
@@ -0,0 +1,16 @@
+﻿namespace Warehouse
+{
+    public class Routing
+    {
+        public string Barcode { get; }
+        public double Weight { get; }
+        public string TargetLane { get; }
+
+        public Routing(string barcode, double weight, string targetLane)
+        {
+            Barcode = barcode;
+            Weight = weight;
+            TargetLane = targetLane;
+        }
+    }
+}
