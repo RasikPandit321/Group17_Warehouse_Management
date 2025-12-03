9895cabfbbdc4cd8b462aa76153377738460c7af
Bdeuba7977 <bdeuba7977@conestogac.on.ca>
2025-12-02 20:29:10 -0500

    Implemented EnergyReporter logic to satisfy tests (TDD GREEN phase) AB#61

---
+++ WareHouse_Management/Environment/EnergySample.cs
@@ -0,0 +1,8 @@
+﻿namespace WareHouse_Management.Environment
+{
+    public class EnergySample
+    {
+        public double Temperature { get; set; }
+        public bool FanOn { get; set; }
+    }
+}
